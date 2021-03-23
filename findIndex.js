e60779bdd185748d996ce33f484b34c90e36d9d4
Dominique Sakrisson <domsakrisson@gmailcom>
2021-03-23 15:01:17 -0700

    passing findIndex test

--- findIndex.js
+++ findIndex.js
@@ -1,18 +1,18 @@
-// const callback = (item) => {
-//     if(item === 6){
-//         return true;
-//     }
-// }
+const callback = (item) => {
+    if(item === 6){
+        return true;
+    }
+}
 
-// const findIndex = ((arr, callback) => {
-//     for(let i = 0; i < arr.length; i++){
-//         if(arr[i] !== null && arr[i] !== undefined){
-//             if(callback(arr[i])) { return i};
-//         }
-//     }
-// })
+const findIndex = ((arr, callback) => {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== null && arr[i] !== undefined){
+            if(callback(arr[i])) { return i};
+        }
+    }
+})
 
-// module.exports = {
-//     callback,
-//     findIndex
-// }
+module.exports = {
+    callback,
+    findIndex
+}
