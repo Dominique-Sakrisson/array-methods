e696fbc4e880224147c3976d558760ed7d2c2a6a
Dominique Sakrisson <domsakrisson@gmailcom>
2021-03-23 14:02:12 -0700

    passing map test with hardcoded data

--- map.test.js
+++ map.test.js
@@ -2,9 +2,9 @@ const {map, callback} = require('./map.js');
 
 describe('array methods', () => {
     it('takes in an array and callback function, iterates over each item and passes the item into the callback function', () => {
-        const arr = [];
+        const arr = [1, 2, 'bill'];
         const items = map(arr, callback);
 
-        expect(items).toEqual();
+        expect(items).toEqual(1, 2, 'bill');
     });
 })
